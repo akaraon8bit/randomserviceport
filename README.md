f8a16cb244879ff5e3229bee7f3c486d08b74540
Ifeanyi Ukpaiyi <ifeanyiukpaiyi@icloud.com>
2023-01-25 18:48:12 -0500

    added persistent port generator

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # randomserviceport
+// this package exist because trying to bind to popular port could terminate the program if it's been used by another program, this progam bruteforce open port then save it in $HOME/.workspace/port/servicename
+//p := randomserviceport.GetPersistentRandomPort("tcp://127.0.0.1:443",".workspace", "443")
+//p := randomserviceport.GetPersistentRandomPort("udp://127.0.0.1:53",".workspace, "nameserver")
+
+//usage = PROXYPORT=`cat $HOME/.workspace/port/443` some program that need the p
+
+
+package main
+
+import
+(
+  "fmt"
+  "github.com/akaraon8bit/randomserviceport"
+)
+
+
+func main() {
+
+  p := randomserviceport.GetPersistentRandomPort("tcp://127.0.0.1:443",".app", "apacheproxy")
+  fmt.Println(p)
--- randomserviceport.go
+++ randomserviceport.go
@@ -16,6 +16,35 @@ import
 )
 
 
+
+
+
+// this package exist because trying to bind to popular port could terminate the program if it's been used by another program, this progam bruteforce open port then save it in $HOME/.workspace/port/servicename
+//p := randomserviceport.GetPersistentRandomPort("tcp://127.0.0.1:443",".workspace", "443")
+//p := randomserviceport.GetPersistentRandomPort("udp://127.0.0.1:53",".workspace, "nameserver")
+
+//usage = PROXYPORT=`cat $HOME/.workspace/port/443` some program that need the port
+
+
+// get persistent port
+func GetPersistentRandomPort(portstring string, workSpace string, serviceName string) (string) {
+
+   servicePort, _ :=  LoadPortFromWorkDir(workSpace, serviceName)
+
+    if servicePort == ""{
+
+      servicePort = GetRandomFreePort(portstring)
+      SavePortToWorkDir(servicePort, workSpace , serviceName, true)
+
+    }
+
+
+  return servicePort
+}
+
+
+
+// get random free port
 func GetRandomFreePort(portstring string) (string) {
 
   OpenPort :=	OpenPortAny(portstring)
@@ -58,7 +87,7 @@ func OpenPortAny( portstring string) (bool) {
 //hostport := host + ":" + port
 switch strings.ToLower(u.Scheme) {
 case "udp":
-
+//this packet query if dns is available on the given port
   e := wait.New(
     wait.WithProto("udp"),
     wait.WithUDPPacket([]byte{0x00,  0x01,  0x01,  0x00,  0x00,  0x01,  0x00,  0x00,  0x00,  0x00,  0x00,  0x00,  0x06,  0x67,  0x6F,  0x6F,  0x67,  0x6C,  0x65,  0x03,  0x63,  0x6F,  0x6D,  0x00,  0x00,  0x0F,  0x00,  0x01, 0x01}),
@@ -102,22 +131,22 @@ func SavePortToWorkDir(texts string, workSpace string, serviceName string, overW
 	usr, _ := user.Current()
 
 	workDir := filepath.Join(usr.HomeDir, workSpace)
-	resultpath := filepath.Join(workDir, "port")
-		if _, err := os.Stat(resultpath); errors.Is(err, os.ErrNotExist) {
-			err := os.Mkdir(resultpath, os.ModePerm)
+  workSpace = filepath.Join(workDir, "port")
+		if _, err := os.Stat( workSpace); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(workSpace, os.ModePerm)
 			if err != nil {
 //				log.Println(err)
 			}
 		}
 
-	resultfilepath := filepath.Join(resultpath,  serviceName)
+	servicepath := filepath.Join(workSpace,  serviceName)
 
 
 	var tmpFile *os.File
 	if overWrite{
-		tmpFile, _ = os.OpenFile(resultfilepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+		tmpFile, _ = os.OpenFile(servicepath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
 	}else{
-		tmpFile, _ = os.OpenFile(resultfilepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		tmpFile, _ = os.OpenFile(servicepath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	}
 
 
@@ -132,4 +161,28 @@ func SavePortToWorkDir(texts string, workSpace string, serviceName string, overW
 	}
 
 	return tmpFile.Name(), nil
+}
+
+
+
+// load save generated port
+
+func LoadPortFromWorkDir(workSpace string, serviceName string) (string, error) {
+
+	usr, _ := user.Current()
+
+	workDir := filepath.Join(usr.HomeDir, workSpace)
+	workSpace = filepath.Join(workDir, "port")
+		if _, err := os.Stat(workSpace); errors.Is(err, os.ErrNotExist) {
+			err := os.Mkdir(workSpace, os.ModePerm)
+			if err != nil {
+//				log.Println(err)
+			}
+		}
+
+	servicepath := filepath.Join(workSpace,  serviceName)
+
+  ports, _:= os.ReadFile(servicepath)
+
+	return string(ports), nil
 }
